5af967759bc58129ea1e063597da46dbf25f2089
robin-safarik-skolni <robin-josef.safarik@gyrab.cz>
2026-04-14 09:19:05 +0200

    zmena

--- inf_hra/hra_hraci.py
+++ inf_hra/hra_hraci.py
@@ -12,7 +12,7 @@ def check_winner(board):
     for row in board:
         if row[0] == row[1] == row[2] and row[0] != ' ':
             return row[0]
-    
+            
     # Check columns
     for col in range(3):
         if board[0][col] == board[1][col] == board[2][col] and board[0][col] != ' ':
